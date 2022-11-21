b3c2737d5df22c3012d60b276bb2aa3236d889db
CptPanther <118699056+CptPanther@users.noreply.github.com>
2022-11-21 04:51:22 +0400

    Add files via upload

---
+++ Lesson_01/Task_01/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+internal class Program
+{
+    static void Main()
+    {
+        WriteLine("Hello World!!!");
+        ReadKey();
+    }
+}
---
+++ Lesson_01/Task_02/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Write("Hello ");
+            Write("World ");
+            Write("!!!");
+            ReadLine();
+        }
+    }
+}
